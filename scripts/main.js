50d8aec7746dd3c7bfbf209cd7ff54c6bb80fdfb
Scavet <131402032+Scavet@users.noreply.github.com>
2024-04-10 00:55:24 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,8 @@
  * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
  * 
  *********************************************************************************/
-
+let inputEcriture = document.getElementById("inputEcriture"), boutonValidation=document.getElementById("btnValiderMot");
+let zoneProposition = document.querySelector(".zoneProposition"), zoneScore = document.querySelector(".zoneScore");
+let listeInputRadio = document.querySelectorAll(".optionSource input");
+console.log(inputEcriture,boutonValidation,zoneProposition,zoneScore,listeInputRadio)
 lancerJeu()
